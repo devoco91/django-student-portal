41f60486deb1c09d6564641a6b1121c52dcaa06d
David <ashuaib1991@gmail.com>
2025-05-17 10:58:37 +0100

    staticfiles removed

--- backend/settings.py
+++ backend/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]  
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
